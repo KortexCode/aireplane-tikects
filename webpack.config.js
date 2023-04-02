bf90b845a576390236ad567268e3ec7bb0eaab20
kortexcode <darkmaeno323@gmail.com>
2023-04-02 13:58:02 -0500

    Cambios en archivos de configuración

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,10 @@ module.exports = {
     resolve: {//Con que extensiones va a trabajar webpack
         extensions:[".js", ".jsx"],
         alias: {
-            "@pages": path.resolve(__dirname, "src/pages"),
             "@components": path.resolve(__dirname, "src/components"),
             "@styles": path.resolve(__dirname, "src/styles"),
+            "@machines": path.resolve(__dirname, "src/machines"),
+            "@containers": path.resolve(__dirname, "src/containers"),
         }
     },
     module: {
